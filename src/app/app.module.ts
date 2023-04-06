9089af268a764afbcc1b93b4d95bca16fe5a474e
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-04-06 22:20:57 +0200

    Added candlestick chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyChartComponent } from './chart/chart.component';
+import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
